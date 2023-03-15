d7c2ea7dc5e2c39fb4b8303f10b452b427406498
shota_niga <shota.niga@informatix.co.jp>
2023-03-15 23:53:28 +0900

    <add> initial commit

--- nodejs/00_socketio_react/client/src/App.tsx
+++ nodejs/00_socketio_react/client/src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
+import {
+  ClientToServerEvents,
+  ClientToServerType,
+  ServerToClientEvents,
+  ServerToClientType,
+} from "./model";
 
 // WebSocketサーバーの接続設定
 const HOST = "localhost";
-const PORT = 5000;
-const socket = io("http://" + HOST + ":" + PORT);
+const PORT = 3000;
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  "http://" + HOST + ":" + PORT
+);
+let messageId = 0;
 
 // Appコンポーネント
 export default function App() {
@@ -13,23 +22,54 @@ export default function App() {
   // 受信メッセージ一覧の配列
   const [receiveMessages, setReceiveMessages] = React.useState<string[]>([]);
 
-  // サーバーからのメッセージを受信する関数
-  socket.on("receiveMessage", (message) => {
-    console.log(message);
-    setReceiveMessages([...receiveMessages, message]);
-    console.log(receiveMessages);
-  });
+  React.useEffect(() => {
+    // WebSocket接続
+    socket.connect();
+
+    // WebSocketの接続開始時
+    const onConnect = () => {
+      console.log("WebSocket is connected.");
+    };
+    socket.on("connect", onConnect);
+
+    // サーバーからのメッセージを受信時の処理を登録
+    const onReceive = (data: ServerToClientType) => {
+      const message = data.id.toString() + ": " + data.message;
+      setReceiveMessages((previous) => [...previous, message]);
+    };
+    socket.on("receiveMessage", onReceive);
+
+    // WebSocketの接続切断時
+    const onDisconnect = () => {
+      console.log("WebSocket is disconnected.");
+    };
+    socket.on("disconnect", onDisconnect);
+
+    // Clean up
+    return () => {
+      console.log("Unmounted");
+      // イベントリスナーの解除
+      socket.off("receiveMessage", onReceive);
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      // WebSocketの切断
+      socket.disconnect();
+    };
+  }, []);
 
   // 送信ボタンクリック時にメッセージをサーバーに送信する関数
-  function sendMessage() {
+  const sendMessage = () => {
     // Inputタグからのテキスト取得
     if (messageRef.current === null) return;
-    const sendMessage = messageRef.current.value;
+    const sendMessage: ClientToServerType = {
+      id: messageId++,
+      message: messageRef.current.value,
+    };
     // サーバーへのメッセージ送信
     if (!String(sendMessage).trim()) return;
     socket.emit("sendMessage", sendMessage);
     messageRef.current.value = "";
-  }
+  };
 
   return (
     <div>
--- nodejs/00_socketio_react/server/src/index.ts
+++ nodejs/00_socketio_react/server/src/index.ts
@@ -1,44 +1,49 @@
 import express from "express";
 import * as http from "http";
 import * as socketio from "socket.io";
+import { ClientToServerEvents, ServerToClientEvents } from "./model";
 
 // WebSocketサーバーの設定
 const HOST = "localhost";
-const PORT = 5000;
+const PORT = 3000;
 
 // 初期化
 const app: express.Express = express();
 const server = http.createServer(app);
-const io = new socketio.Server(server, {
-  cors: {
-    origin: "*", // クロスサイトの許容
-    credentials: true,
-  },
-});
-
-// WebSocketサーバーの起動
-server.listen(PORT, HOST, () => {
-  console.log("Running at localhost:" + PORT);
-});
+const io = new socketio.Server<ClientToServerEvents, ServerToClientEvents>(
+  server,
+  {
+    cors: {
+      origin: "*", // クロスサイトの許容
+      credentials: true,
+    },
+  }
+);
 
 // WebSocket接続数
 let connectionNum = 0;
 
+// WebSocketサーバーの起動
+server.listen(PORT, HOST, () => {
+  console.log("Server running at localhost:" + PORT);
+  console.log("connection num: " + connectionNum);
+});
+
 // 接続確立時
 io.on("connection", (socket) => {
   connectionNum++;
-  console.log("connection(" + connectionNum + ")");
+  console.log("connection num: " + connectionNum);
 
   // クライアントからのメッセージ受信時
-  socket.on("sendMessage", (message: string) => {
-    console.log("Message has been sent: ", message);
+  socket.on("sendMessage", (data) => {
+    console.log("Message has been sent: ", data.message);
     // クライアントへのメッセージ送信
-    io.emit("receiveMessage", message); // 全クライアントに対して受信したメッセージを送信する
+    io.emit("receiveMessage", data); // 全クライアントに対して受信したメッセージを送信する
   });
 
   // 接続終了時
   socket.on("disconnect", () => {
     connectionNum--;
-    console.log("connection(" + connectionNum + ")");
+    console.log("connection num: " + connectionNum);
   });
 });
