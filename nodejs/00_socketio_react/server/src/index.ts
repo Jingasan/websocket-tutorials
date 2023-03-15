ca8925d25376d8e3ddd1c48520259ac991da1957
shota_niga <shota.niga@informatix.co.jp>
2023-03-15 21:32:35 +0900

    <add> initial commit

--- nodejs/00_socketio_react/server/src/index.ts
+++ nodejs/00_socketio_react/server/src/index.ts
@@ -2,33 +2,43 @@ import express from "express";
 import * as http from "http";
 import * as socketio from "socket.io";
 
+// WebSocketサーバーの設定
+const HOST = "localhost";
+const PORT = 5000;
+
 // 初期化
 const app: express.Express = express();
 const server = http.createServer(app);
 const io = new socketio.Server(server, {
   cors: {
-    origin: "http://localhost:3000", // クロスサイトの許容
+    origin: "*", // クロスサイトの許容
     credentials: true,
   },
 });
 
-// WebSocketサーバーの設定
-const HOST = "localhost";
-const PORT = 5000;
 // WebSocketサーバーの起動
 server.listen(PORT, HOST, () => {
   console.log("Running at localhost:" + PORT);
 });
 
-// コネクション確立時
-io.on("connection", (socket) => {
-  // クライアントとのコネクションが確立したら以下を表示
-  console.log("user connected");
+// WebSocket接続数
+let connectionNum = 0;
 
-  // クライアントからのメッセージ受信
+// 接続確立時
+io.on("connection", (socket) => {
+  connectionNum++;
+  console.log("connection(" + connectionNum + ")");
+
+  // クライアントからのメッセージ受信時
   socket.on("sendMessage", (message: string) => {
     console.log("Message has been sent: ", message);
     // クライアントへのメッセージ送信
     io.emit("receiveMessage", message); // 全クライアントに対して受信したメッセージを送信する
+  });
+
+  // 接続終了時
+  socket.on("disconnect", () => {
+    connectionNum--;
+    console.log("connection(" + connectionNum + ")");
   });
 });
