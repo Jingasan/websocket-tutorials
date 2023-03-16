cde10772962f1b896e22e2a08ef36fe266f598cb
shota_niga <shota.niga@informatix.co.jp>
2023-03-16 19:51:05 +0900

    <add> initial commit

--- nodejs/00_socketio_react/websocket_types/src/index.ts
+++ nodejs/00_socketio_react/websocket_types/src/index.ts
@@ -1,1 +1,17 @@
-export * from "./model";
+// サーバーからクライアントへのメッセージ送信の型
+export type ServerToClientType = {
+  id: number;
+  message: string;
+};
+export type ServerToClientEvents = {
+  receiveMessage: (data: ServerToClientType) => void;
+};
+
+// クライアントからサーバーへのメッセージ送信の型
+export type ClientToServerType = {
+  id: number;
+  message: string;
+};
+export type ClientToServerEvents = {
+  sendMessage: (data: ClientToServerType) => void;
+};
